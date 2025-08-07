e564a10ac8e1133e2f6d8e69e4837cc5e761daaf
jbohigues <jbohigues@vidal-ac.es>
2025-08-07 22:01:24 +0200

    feat: :heavy_plus_sign: add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "server",
+  integrations: [icon()],
 });
